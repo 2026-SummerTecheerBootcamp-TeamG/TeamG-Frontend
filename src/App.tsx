61ba85abff4e3069be79257d571b1ed336e92cc0
yunseol12334 <yunseol2132@gmail.com>
2026-07-14 17:09:07 +0900

    feat: 로그인/회원가입 라우팅 연결 (Closes #17)

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,20 @@ import AppLayout from "@/components/layout/AppLayout";
 import ProtectedRoute from "@/components/layout/ProtectedRoute";
 import { AuthProvider } from "@/context/AuthContext";
 import PlanningRoom from "@/screens/PlanningRoom";
+import Login from "@/screens/login";
+import Signup from "@/screens/signup";
 
 export default function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route element={<AppLayout />}>
-          {/* 메인: 랜딩 + 챗 + 계획서  (이슈 2·4·5) */}
+          {/* 메인: 랜딩 + 챗 + 계획서  (이슈 4·5) */}
           <Route path="/" element={<PlanningRoom />} />
 
-          {/* 로그인 / 회원가입  (이슈 3) */}
-          {/* <Route path="/login" element={<Login />} /> */}
-          {/* <Route path="/signup" element={<Signup />} /> */}
+          {/* 로그인 / 회원가입 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
           {/* 계획서 확정 화면  (이슈 5) */}
           {/* <Route path="/plan/:tripId" element={<PlanConfirm />} /> */}
